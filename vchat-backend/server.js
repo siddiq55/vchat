a8ff35e480cda4615252da1cacadbd8df19887ff
siddiq55 <msiddiq5565@gmail.com>
2026-03-07 13:21:26 +0500

    server

--- vchat-backend/server.js
+++ vchat-backend/server.js
@@ -41,7 +41,7 @@ app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 // Routes setup
-app.get("/", async (req, res) => {
+app.get("/", async (_, res) => {
   const mongoStatus = await connectDB();
   res.json({
     message: "Backend is running on Vercel 🚀",
